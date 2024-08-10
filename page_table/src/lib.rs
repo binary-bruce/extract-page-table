0f3658cfe0523325b8bdbf620ce5f46a140f2209
binary.bruce <myth.litao@foxmail.com>
2024-08-10 15:45:37 +0800

    address module

--- page_table/src/lib.rs
+++ page_table/src/lib.rs
@@ -1,6 +1,14 @@
 #![no_std]
 
+mod addr;
+mod config;
+mod page_table_entry;
+mod pte_flags;
+
 extern crate alloc;
+
+pub use crate::page_table_entry::PageTableEntry;
+pub use crate::pte_flags::PTEFlags;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -14,5 +22,8 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+
+        let a = PTEFlags::V;
+        assert_eq!(PTEFlags::V, a);
     }
 }
