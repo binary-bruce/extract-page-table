d85c8e7545bfbc9ce00e5710e0574fa77d40fb4b
binary.bruce <myth.litao@foxmail.com>
2024-08-10 16:35:18 +0800

    use page table in its own git repo

--- page_table/src/frame/frame_allocator_trait.rs
+++
@@ -1,7 +0,0 @@
-use crate::addr::PhysPageNum;
-
-pub(crate) trait FrameAllocator {
-    fn new() -> Self;
-    fn alloc(&mut self) -> Option<PhysPageNum>;
-    fn dealloc(&mut self, ppn: PhysPageNum);
-}
