dfe15ff947e74798a4cae48be4bb85d8d2a65064
binary.bruce <myth.litao@foxmail.com>
2024-08-11 14:21:34 +0800

    apply builder for user memory set

--- os/src/mm/user_memory_set.rs
+++ os/src/mm/user_memory_set.rs
@@ -3,7 +3,8 @@
 use crate::config::{TRAMPOLINE, TRAP_CONTEXT, USER_STACK_SIZE};
 
 use memory_set::*;
-use page_table::{PhysAddr, VirtAddr, VirtPageNum, PAGE_SIZE};
+use page_table::{VirtAddr, VirtPageNum, PAGE_SIZE};
+use xmas_elf::program::Flags;
 
 extern "C" {
     fn strampoline();
@@ -12,12 +13,9 @@ extern "C" {
 /// Include sections in elf and trampoline and TrapContext and user stack,
 /// also returns user_sp and entry point.
 pub fn from_elf(elf_data: &[u8]) -> (MemorySet, usize, usize) {
-    let mut memory_set = MemorySet::new_bare();
-    // map trampoline
-    memory_set.map_trampoline(
-        VirtAddr::from(TRAMPOLINE).into(),
-        PhysAddr::from(strampoline as usize).into(),
-    );
+    let mut memory_set_builder =
+        MemorySetBuilder::new().map_trampoline(TRAMPOLINE, strampoline as usize);
+
     // map program headers of elf, with U flag
     let elf = xmas_elf::ElfFile::new(elf_data).unwrap();
     let elf_header = elf.header;
@@ -30,21 +28,14 @@ pub fn from_elf(elf_data: &[u8]) -> (MemorySet, usize, usize) {
         if ph.get_type().unwrap() == xmas_elf::program::Type::Load {
             let start_va: VirtAddr = (ph.virtual_addr() as usize).into();
             let end_va: VirtAddr = ((ph.virtual_addr() + ph.mem_size()) as usize).into();
-            let mut map_perm = MapPermission::U;
-            let ph_flags = ph.flags();
-            if ph_flags.is_read() {
-                map_perm |= MapPermission::R;
-            }
-            if ph_flags.is_write() {
-                map_perm |= MapPermission::W;
-            }
-            if ph_flags.is_execute() {
-                map_perm |= MapPermission::X;
-            }
+            let map_perm = get_map_perm(ph.flags());
             let map_area = MapArea::new(start_va, end_va, MapType::Framed, map_perm);
             max_end_vpn = map_area.vpn_range.get_end();
-            memory_set.push(
-                map_area,
+
+            memory_set_builder = memory_set_builder.push_framed_with_data(
+                start_va.into(),
+                end_va.into(),
+                map_perm,
                 Some(&elf.input[ph.offset() as usize..(ph.offset() + ph.file_size()) as usize]),
             );
         }
@@ -55,38 +46,33 @@ pub fn from_elf(elf_data: &[u8]) -> (MemorySet, usize, usize) {
     // guard page
     user_stack_bottom += PAGE_SIZE;
     let user_stack_top = user_stack_bottom + USER_STACK_SIZE;
-    memory_set.push(
-        MapArea::new(
-            user_stack_bottom.into(),
-            user_stack_top.into(),
-            MapType::Framed,
-            MapPermission::R | MapPermission::W | MapPermission::U,
-        ),
-        None,
-    );
-    // used in sbrk
-    memory_set.push(
-        MapArea::new(
-            user_stack_top.into(),
-            user_stack_top.into(),
-            MapType::Framed,
-            MapPermission::R | MapPermission::W | MapPermission::U,
-        ),
-        None,
-    );
-    // map TrapContext
-    memory_set.push(
-        MapArea::new(
-            TRAP_CONTEXT.into(),
-            TRAMPOLINE.into(),
-            MapType::Framed,
-            MapPermission::R | MapPermission::W,
-        ),
-        None,
-    );
+
+    let rwu = MapPermission::R | MapPermission::W | MapPermission::U;
+    let rw = MapPermission::R | MapPermission::W;
+    let memory_set = memory_set_builder
+        .push_framed(user_stack_bottom, user_stack_top, rwu)
+        .push_framed(user_stack_top, user_stack_top, rwu)
+        .push_framed(TRAP_CONTEXT, TRAMPOLINE, rw)
+        .build();
+
     (
         memory_set,
         user_stack_top,
         elf.header.pt2.entry_point() as usize,
     )
+}
+
+fn get_map_perm(ph_flags: Flags) -> MapPermission {
+    let mut map_perm = MapPermission::U;
+    if ph_flags.is_read() {
+        map_perm |= MapPermission::R;
+    }
+    if ph_flags.is_write() {
+        map_perm |= MapPermission::W;
+    }
+    if ph_flags.is_execute() {
+        map_perm |= MapPermission::X;
+    }
+
+    map_perm
 }
